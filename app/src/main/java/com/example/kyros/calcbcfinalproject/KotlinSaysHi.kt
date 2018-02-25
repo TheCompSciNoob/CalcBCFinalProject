1da85f9122b34d4a74be0407c4468c7e6503d5ba
TheCompSciNoob <kyroschow@gmail.com>
2018-02-24 23:51:43 -0800

    Kotlin Says Hi

---
+++ app/src/main/java/com/example/kyros/calcbcfinalproject/KotlinSaysHi.kt
@@ -0,0 +1,7 @@
+package com.example.kyros.calcbcfinalproject
+
+/**
+ * Created by Kyros on 2/24/2018.
+ */
+data class KotlinSaysHi(var intro : String = "Hi", var message : String = "Hello World") {
+}
