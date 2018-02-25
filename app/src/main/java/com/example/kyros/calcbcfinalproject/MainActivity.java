af4242e9c95e8184a6a4932db661e9797b3e22d9
TheCompSciNoob <kyroschow@gmail.com>
2018-02-25 09:35:43 -0800

    Update SDK

--- app/src/main/java/com/example/kyros/calcbcfinalproject/MainActivity.java
+++ app/src/main/java/com/example/kyros/calcbcfinalproject/MainActivity.java
@@ -27,7 +27,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Rep" +
+                        "lace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
