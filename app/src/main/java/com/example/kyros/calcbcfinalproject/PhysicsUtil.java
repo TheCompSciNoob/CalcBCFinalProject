b49697426fa6a54197f6430a5520e183748f3c46
phoebekim <phoebekim99@gmail.com>
2018-04-03 11:47:00 -0700

    think i finished getting velocity and acceleration

---
+++ app/src/main/java/com/example/kyros/calcbcfinalproject/PhysicsUtil.java
@@ -0,0 +1,32 @@
+package com.example.kyros.calcbcfinalproject;
+
+public class PhysicsUtil {
+    private String x, y;
+    private double a, b, c, d, e, f;
+
+    public PhysicsUtil(String x, String y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    //assigning values to a, b, c, d, e, and f
+    public void parse()
+    {
+        a = Double.parseDouble(x.substring(0, x.indexOf("t^2")));
+        b = Double.parseDouble(x.substring(x.indexOf("t^2")+4, x.indexOf("t+")));
+        c = Double.parseDouble(x.substring(x.indexOf("t+")+2, x.length()));
+        d = Double.parseDouble(y.substring(0, y.indexOf("t^2")));
+        e = Double.parseDouble(y.substring(y.indexOf("t^2")+4, y.indexOf("t+")));
+        f = Double.parseDouble(y.substring(y.indexOf("t+")+2, y.length()));
+    }
+
+    public double getVelocity(double t)
+    {
+        return ((2 * d * t) + e) / ((2 * a * t) + b);
+    }
+
+    public double getAcceleration(double t)
+    {
+        return ((((2 * a * t) + b) * (2 * d)) - (((2 * d * t) + e) * (2 * a))) / Math.pow(((2 * a * t) + b), 3);
+    }
+}
