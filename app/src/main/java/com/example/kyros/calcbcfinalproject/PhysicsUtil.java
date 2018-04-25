53c6281fade9e63a0be0d6f8160f3c9939ddda32
Angela <angelazhang331@gmail.com>
2018-04-25 13:32:06 -0700

    readable

--- app/src/main/java/com/example/kyros/calcbcfinalproject/PhysicsUtil.java
+++ app/src/main/java/com/example/kyros/calcbcfinalproject/PhysicsUtil.java
@@ -1,24 +1,28 @@
 package com.example.kyros.calcbcfinalproject;
 
 public class PhysicsUtil {
-    private String x, y;
+//    private String x, y;
     private double a, b, c, d, e, f;
 
-    public PhysicsUtil(String x, String y)
+    public PhysicsUtil(double a, double b, double c, double d, double e, double f)
     {
-        this.y = y;
-        this.x = x;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
     }
     //assigning values to a, b, c, d, e, and f
-    public void parse()
-    {
-        a = Double.parseDouble(x.substring(0, x.indexOf("t^2")));
-        b = Double.parseDouble(x.substring(x.indexOf("t^2")+4, x.indexOf("t+")));
-        c = Double.parseDouble(x.substring(x.indexOf("t+")+2, x.length()));
-        d = Double.parseDouble(y.substring(0, y.indexOf("t^2")));
-        e = Double.parseDouble(y.substring(y.indexOf("t^2")+4, y.indexOf("t+")));
-        f = Double.parseDouble(y.substring(y.indexOf("t+")+2, y.length()));
-    }
+//    public void parse()
+//    {
+//        a = Double.parseDouble(x.substring(0, x.indexOf("t^2")));
+//        b = Double.parseDouble(x.substring(x.indexOf("t^2")+4, x.indexOf("t+")));
+//        c = Double.parseDouble(x.substring(x.indexOf("t+")+2, x.length()));
+//        d = Double.parseDouble(y.substring(0, y.indexOf("t^2")));
+//        e = Double.parseDouble(y.substring(y.indexOf("t^2")+4, y.indexOf("t+")));
+//        f = Double.parseDouble(y.substring(y.indexOf("t+")+2, y.length()));
+//    }
 
     public double getVelocity(double t)
     {
@@ -37,11 +41,16 @@ public class PhysicsUtil {
         //TODO: check if correct
         for (int i = 0; i < 99; i++)
         {
-            double w = (t * ((i+1) / 100));
-            yValues += (2 * java.lang.Math.sqrt((4 * a * a * w * w) + (4 * a * b * w) + (b * b) + (4 * d * d * w * w) + (4 * d * e * w) + (e * e)));
+            double tempTime = (t * ((i+1) / 100));
+            double dx = 2 * a * tempTime + b;
+            double dy = 2 * d * tempTime + e;
+            yValues += (2 * java.lang.Math.sqrt(dx * dx + dy * dy));
         }
 
+
+        //first term
         yValues += java.lang.Math.sqrt((4 * a * a * 0 * 0) + (4 * a * b * 0) + (b * b) + (4 * d * d * 0 * 0) + (4 * d * e * 0) + (e * e));
+        //last term
         yValues += java.lang.Math.sqrt((4 * a * a * t * t) + (4 * a * b * t) + (b * b) + (4 * d * d * t * t) + (4 * d * e * t) + (e * e));
 
         return (0.5 * (t / 100) * (yValues));
